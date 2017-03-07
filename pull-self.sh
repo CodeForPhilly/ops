11aea5db5a31f5435d2e7b53afca75f7a18a8c6b
James Lott <jlott@linode.com>
2017-03-07 18:04:45 -0500

    Log more output from cron based pulls

--- pull-self.sh
+++ pull-self.sh
@@ -5,11 +5,10 @@ self_real=$(readlink -f "$0")
 self_base=$(dirname "$self_real")
 
 cd "$self_base"
-git remote update
+git remote update | logger --id -t "$self_base"
 distance=$(git status -uno | grep '^Your branch is behind' | head -n1)
 
 if [ -n "$distance" ]; then
   logger --id -t "$self_base" "$distance"
-  action=$(git pull | grep '^Updating' | head -n1)
-  logger --id -t "$self_base" "$action"
+  git pull | logger --id -t "$self_base"
 fi
