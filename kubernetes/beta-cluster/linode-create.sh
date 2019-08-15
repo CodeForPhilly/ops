4cb3cf49c36ee7bc73d06780ef097327a37ce87d
Chris Alfano <chris@jarv.us>
2019-08-15 00:34:03 +0000

    fix: rename cluster to beta-cluster

--- kubernetes/beta-cluster/linode-create.sh
+++ kubernetes/beta-cluster/linode-create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-linode-cli k8s-alpha create codeforphilly \
+linode-cli k8s-alpha create beta-cluster \
     --region us-east \
     --master-type g6-standard-2 \
     --node-type g6-standard-2 \
