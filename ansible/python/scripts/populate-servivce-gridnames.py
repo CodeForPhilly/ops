b090d46e681648bb7d6d3ec2fffb35b7dae3c420
James Lott <james@lottspot.com>
2020-06-19 13:11:54 -0400

    Populate service grid names

---
+++ ansible/python/scripts/populate-servivce-gridnames.py
@@ -0,0 +1,22 @@
+import sys
+import json
+
+inputs = json.loads(sys.stdin.read())
+
+project_name = inputs['project_name']
+project_svcs = inputs['services']
+
+outputs = []
+
+if __name__ == '__main__':
+
+    for svc in project_svcs:
+        group_pfx = '{}.{}'.format(svc['name'], project_name)
+        svc['groups'] = [
+            {'scope': 'global' , 'name': '{}.cloud.domain'.format(group_pfx) },
+            {'scope': 'node'   , 'name': '{}.node.domain'.format(group_pfx)  },
+        ]
+        outputs.append(svc)
+
+    sys.stdout.write(json.dumps(outputs))
+    sys.exit(0)
